6766efd08ceda41ae9fbdc6bdcc0079fd099d3f7
Spencer Eanes <eanes1@stolaf.edu>
2018-05-13 16:57:45 -0500

    accidentally deleted files in last commit

---
+++ src/quadtree.h
@@ -0,0 +1,59 @@
+#ifndef _quadtree_
+#define _quadtree_
+#include <iostream>
+#include <fstream>
+#include "bodygroup.h"
+using namespace std;
+
+class quadtree{
+  protected:
+    bool parent;
+    body* contained;
+    
+    long double comX;
+    long double comY;
+    long double total_mass;
+    long double* comVals;
+    
+    float theta;    
+
+    quadtree** children;
+    
+    long double left, right;
+    long double top, bottom;
+
+  public:
+    quadtree();
+    quadtree(long double l, long double r, long double t, long double b, body* c);
+    //quadtree(const bodygroup&);//not working and I dont think we'll need it.
+    ~quadtree();
+
+    void insert(body*);
+    void insert(bodygroup*);
+
+    long getLeft(){return left;}
+    long getRight() {return right; }
+    long getTop() { return top;}
+    long getBottom() { return bottom; }
+
+    float getTheta() { return theta; }
+    void setTheta(float t) { theta = t; }
+    
+    long double getDim() { return ((right-left) + (top-bottom))/2; }
+
+    long double getComX() { return comX; }
+    long double getComY() { return comY; }
+    long double getMass() { return total_mass; }
+    void updateComVals();
+
+    bool empty() { return (!parent && contained == NULL); }
+    bool isParent() { return parent; }
+    int whichChild(body*);
+    quadtree* getChild(int index) { return children[index]; }
+
+    long double* calcCOM();
+    void clear(); 
+
+    void barnesHut(body*);
+};
+#endif //_quadtree_
---
+++ src/quadtreedriver.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include "quadtree.h"
+#include "elapsed_time.h"
+using namespace std;
+
+int main(){
+  
+
+  bodygroup bg(cin);
+  //cout << "Number of time steps: ";
+  int time_steps = 100;
+  //cin >> time_steps;
+  double avg_constr = 0;
+  double avg_com = 0;
+  double avg_comp = 0;
+  for(int i = 0; i < time_steps; i++){
+
+    //bg.display();
+    //cout << endl;
+    //cout << "getting to the end" << endl;
+    start_timer();
+    quadtree q1;
+    q1.setTheta(1);
+    q1.insert(&bg);
+    avg_constr+=elapsed_time();
+    //with too many particles it is not getting past inserting them all
+    cout << i << endl; 
+     
+    start_timer();
+    q1.calcCOM();
+    avg_com += elapsed_time();
+    //this is where we parallelize
+    //update the acclerations of all bodies
+    
+    //start_timer();
+    for(int i = 0; i < bg.getSize(); i++){
+      q1.barnesHut(bg[i]);  
+    }
+    //once all accels are computed, then
+    //we can update positions
+    for(int i = 0; i < bg.getSize(); i++){
+      bg[i]->update();
+    }
+    //avg_comp += elapsed_time();
+  }
+  bg.display();
+
+  cout << "Average construction time: " << avg_constr/time_steps << endl;
+  cout << "Average center of mass calculation time: " << avg_com/time_steps << endl;
+  cout << "Average BH computations: " << avg_comp/time_steps << endl;
+
+  
+  return 0;
+}
