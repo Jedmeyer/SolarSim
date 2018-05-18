d8cefeac2fef6f677166ffcf8d49deab430471e0
Jedmeyer <jedmeyer@stolaf.edu>
2018-05-17 23:36:24 -0500

    Fixing stuff

--- readin/proto-draw.cpp
+++ readin/proto-draw.cpp
@@ -26,12 +26,13 @@ int numFrames;
 int numCircs;
 int dampener= 0;
 int DAMPENER;
+int helper = 0;
 
 //necessary definitions for the pause/play button
 bool pausePress = false, overPause = false;
 float pausePos[] = {(int)(WIDTH/2)-52, HEIGHT-52, 52, 52};
-char unpausedtext[] = "||";//text displayed on the button if simulation is not paused
-char pausedtext[] = ">";//text displayed on button if simulation is paused
+char unpausedtext[] = ">";//text displayed on the button if simulation is not paused
+char pausedtext[] = "||";//text displayed on button if simulation is paused
 char * pausebuttonname = unpausedtext; //pointer to change the text displayed on the pause button when it is clicked
 int pauseobject = 0;
 
@@ -40,6 +41,13 @@ bool resetPress = false, overReset = false;
 float resetPos[] = {(int)(WIDTH/2)+4, HEIGHT-52, 52, 52};
 char resetText[] = "R";
 
+//slider
+bool sliderIsPressed = false, overSlider = false;
+double sliderBox1[] = { (int)(WIDTH-100)/2, HEIGHT-26,   100, 4 };  // background of slider
+double sliderBox2[] = {  (int)(WIDTH-100)/2+5, HEIGHT-18,   10, 20 };  // foreground of slider
+double sliderPos = 0;  // where the slider currently is located
+double sliderStartPos;  // where the mouse first clicked on the slider
+double sliderMax = 100;  // upper limit to the sliderPos
 
 //draw text function to label buttons
 void drawText(float x, float y, const char *text) {
@@ -47,6 +55,9 @@ void drawText(float x, float y, const char *text) {
   int length = strlen(text);
   for (int i = 0; i < length; i++)
     glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, text[i]);
+}
+void drawText(float *pos, const char *text) {
+  drawText(pos[0], pos[1], text);
 }
 //draws a rectangle, giver coords of upper left corner, width, height
 void drawBox(float x, float y, float width, float height) {
@@ -64,6 +75,23 @@ void drawBox(float *pos) {
   drawBox(pos[0], pos[1], pos[2], pos[3]);
 }
 
+// the following function draws a rectangle, given
+//   the upper left vertex and the width and height
+void drawBox(double x, double y, double width, double height)
+{
+  glBegin(GL_POLYGON);
+    glVertex2f(x, y);  // upper left
+    glVertex2f(x, y + height);  // lower left
+    glVertex2f(x + width, y + height);  // lower right
+    glVertex2f(x + width, y);  // upper right
+  glEnd();
+}
+void drawBox(double *pos)
+{
+  drawBox(pos[0], pos[1], pos[2], pos[3]);
+}
+
+
 void loadCircs(const char* filename)
 {
   ifstream g(filename);
@@ -78,8 +106,8 @@ void loadCircs(const char* filename)
     ciroc[i] = new Circle[numCircs];
     for(int j = 0; j < numCircs; j++)
       {
-	ciroc[i][j] = Circle(g);
-	
+  ciroc[i][j] = Circle(g);
+  
       }
   }
   
@@ -91,24 +119,18 @@ void drawWindow()
 {
   // clear the buffer
   glClear(GL_COLOR_BUFFER_BIT);
-  // draw stuff
-  /*
-  for(int i = 0; i < numUserShapes; i++)
-      userShape[i]->draw();
-  
-  for(int j = 0; j < numColorButtons; j++)
-    {
-      colorButtonBackground[j]->draw();
-      colorButtonForeground[j]->draw();
-    }
-  */
+
   
   for (int i = 0; i<numCircs; i++)
     ciroc[ct][i].draw();
   if (dampener%DAMPENER == 0) {
     if (ct < numFrames-2)
-      if (pauseobject == 1)
-	ct++;
+      if (pauseobject == 1){
+  ct++;
+  if (helper%(numFrames/100) == 0)
+    sliderPos++;
+  helper++;
+      }
   }
     if (dampener == DAMPENER)
       dampener = 0;
@@ -120,7 +142,7 @@ void drawWindow()
   else glColor3f(.5,.5,.5);
   drawBox(pausePos);
   glColor3f(1., 0., 0.);
-  drawText((int)(WIDTH/2)-31, HEIGHT-20, pausebuttonname);
+  drawText((int)(WIDTH/2)-31, HEIGHT-80, pausebuttonname);
 
   //Draws the reset button
   if (resetPress) glColor3f(.75, 1., .4);
@@ -128,7 +150,17 @@ void drawWindow()
   else glColor3f(.5, .5, .5);
   drawBox(resetPos);
   glColor3f(1., 0., 0.);
-  drawText((int)(WIDTH/2)+26, HEIGHT-20, resetText);
+  drawText((int)(WIDTH/2)+26, HEIGHT-80, resetText);
+
+    // draw the slider background
+  if ( sliderIsPressed || overSlider ) glColor3f(.5, .5, .5);  // gray
+  else glColor3f(.25, .25, .25);  // dark gray
+  drawBox(sliderBox1);
+  // draw the slider foreground
+  if ( overSlider ) glColor3f(.95, .95, .95);  // very light gray
+  else glColor3f(.75, .75, .75);  // light gray
+  drawBox(sliderBox2[0] + sliderPos, sliderBox2[1], 
+          sliderBox2[2],  sliderBox2[3]);
   
   // tell the graphics card that we're done-- go ahead and draw!
   //   (technically, we are switching between two color buffers...)
@@ -174,6 +206,13 @@ void reshape(int w, int h)
    glMatrixMode(GL_PROJECTION);
    glLoadIdentity();
    glOrtho(0., WIDTH-1, HEIGHT-1, 0., -1.0, 1.0);
+
+   pausePos[0] = (int)(WIDTH/2)-52; pausePos[1] =  HEIGHT-110;
+   resetPos[0] = (int)(WIDTH/2)+9; resetPos[1] = HEIGHT-110;
+   sliderBox1[0] = (int)(WIDTH-100)/2+5; sliderBox1[1] = HEIGHT-26; sliderBox1[2] = 100;
+   sliderBox2[0] =  (int)(WIDTH-100)/2+5; sliderBox2[1] =  HEIGHT-39;
+   sliderMax = numFrames;
+
 }
 bool onPause(int x, int y) {
   return x >= pausePos[0] && y >= pausePos[1] && x <=pausePos[0] + pausePos[2] && y <=pausePos[1] + pausePos[3];
@@ -181,6 +220,21 @@ bool onPause(int x, int y) {
 
 bool onReset(int x, int y) {
   return x >= resetPos[0] && y >= resetPos[1] && x <=resetPos[0] + resetPos[2] && y <=resetPos[1] + resetPos[3];
+}
+
+// the following function tests whether a point at position x,y is inside
+//   the rectangle on the screen corresponding to the slider
+bool onSliderForeground(int x, int y)
+{
+  return x >= sliderBox2[0]+sliderPos  && y >= sliderBox2[1] &&
+         x <= sliderBox2[0]+sliderBox2[2]+sliderPos &&
+         y <= sliderBox2[1]+sliderBox2[3];
+}
+bool onSliderBackground(int x, int y)
+{
+  return x >= sliderBox2[0]  && y >= sliderBox2[1] &&
+         x <= sliderBox2[0]+sliderMax+sliderBox2[2] &&
+         y <= sliderBox2[1]+sliderBox2[3];
 }
 
 // the mouse function is called when a mouse button is pressed down or released
@@ -193,26 +247,40 @@ void mouse(int button, int state, int x, int y)
       else pausePress = false;
       if (onReset(x, y) ) resetPress = true;
       else resetPress = false;
-
+      if ( onSliderForeground(x,y) ) {
+        sliderIsPressed = true;
+        sliderStartPos = x - sliderPos;
+      } else if ( onSliderBackground(x,y) ) {
+        sliderIsPressed = true;
+        sliderPos = x - sliderBox1[0];
+        sliderStartPos = x-sliderPos;
+      } else sliderIsPressed = false;
     } 
     else { 
       // the user just let go the mouse-- do something
-      if (onPause(x, y) && pausePress) { 	if (pauseobject == 0) {
-	  pauseobject = 1;//pauses the simulation
-	  pausePress = false;
-	  pausebuttonname = pausedtext;
-	} 	
-	else {
-	  pauseobject = 0;//unpauses the simulation
-	  pausePress = false; //alternate way to note that the display is paused is by not declaring pausePress to be false unless the button is clicked again, playing the simulation. this would leave the button highlighted.
-	  pausebuttonname = unpausedtext;//switches text displayed over the pause button to note whether or not the simulation is running
-	}
+      if (onPause(x, y) && pausePress) {  if (pauseobject == 0) {
+    pauseobject = 1;//pauses the simulation
+    pausePress = false;
+    pausebuttonname = pausedtext;
+  }   
+  else {
+    pauseobject = 0;//unpauses the simulation
+    pausePress = false; //alternate way to note that the display is paused is by not declaring pausePress to be false unless the button is clicked again, playing the simulation. this would leave the button highlighted.
+    pausebuttonname = unpausedtext;//switches text displayed over the pause button to note whether or not the simulation is running
+  }
       }
-      if (onReset(x, y) && resetPress) {
-	ct = 0;
-	resetPress = false;
-      }
+      if ( sliderIsPressed )
+        //cout << "New slider position: " << sliderPos << endl;
+  if (sliderPos < 100)
+    ct = sliderPos * (numFrames/100);
+      
+      sliderIsPressed = false;
     }
+      if (onReset(x, y) && resetPress) {
+  ct = 0;
+  sliderPos = 0;
+  resetPress = false;
+      }
   } else if ( GLUT_RIGHT_BUTTON == button ) {
   }
   glutPostRedisplay();
@@ -227,6 +295,18 @@ void mouse_motion(int x,int y)
   else overPause = false;
   if(onReset(x,y)) overReset = true;
   else overReset = false;
+
+  if ( sliderIsPressed ) {
+    double newSliderPos = x - sliderStartPos;
+    // check that the new slider pos does not go off the end of the slider
+    if ( newSliderPos < 0 ) sliderPos = 0;
+    else if ( newSliderPos > sliderMax ) sliderPos = sliderMax;
+    else sliderPos = newSliderPos;
+  }
+  else {
+    if ( onSliderBackground(x,y) ) overSlider = true;
+    else overSlider = false;
+  }
   // the mouse button is pressed, and the mouse is moving....
   glutPostRedisplay();
 }
@@ -257,7 +337,7 @@ void init(void)
 void init_gl_window()
 {
   ct = 0;
-  loadCircs("../output/test3.txt");
+  loadCircs("../output/test4.txt");
   char *argv[] = {programName};
   int argc = sizeof(argv) / sizeof(argv[0]);
   glutInit(&argc, argv);
